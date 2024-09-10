127b5918a7b2e5125cd88c440263411fe833675b
sotoler <142556630+sotoler@users.noreply.github.com>
2024-09-10 18:14:53 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -41,6 +41,7 @@ let RproxyIP = 'false';
 let proxyIPs = [//无法匹配到节点名就随机分配以下ProxyIP域名
 	'proxyip.multacom.fxxk.dedyn.io',
 	'proxyip.vultr.fxxk.dedyn.io',
+	'my-telegram-is-herocore.onecf.eu.org',
 ];
 let CMproxyIPs = [
 	//'proxyip.aliyun.fxxk.dedyn.io:HK',//匹配节点名, 有HK就分配该ProxyIP域名
